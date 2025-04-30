3e0e8b31abce8fc082b6dd37ddd1b0e6c2c06972
Bhanudprasad <itzbhnauprasad12@gmail.com>
2025-04-30 21:05:37 +0530

    Completed Lab Boolean Operators

--- src/main/lab.py
+++ src/main/lab.py
@@ -12,4 +12,4 @@ def even_or_odd(num):
     :param num: An integer number to check.
     :return: 'Even' if the number is even, 'Odd' if the number is odd.
     """
-    return ''
+    return 'Even' if num % 2 ==0 else 'Odd'
